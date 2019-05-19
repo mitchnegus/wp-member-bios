be8fd09257d5220a5fbb264458c1749ead4e4b8c
Science Policy Group at Berkeley <sciencepolicy@ocf.berkeley.edu>
2019-05-19 00:20:45 -0700

    resolved issue (database related, not cleaning well on deactivation)

--- includes/define-new-members-form.php
+++ includes/define-new-members-form.php
@@ -5,11 +5,12 @@
 
 global $PLUGIN_DIR;
 global $new_member_form_template, $new_member_form_title;
-global $new_member_confirmation_template; $new_member_confirmation_title;
+global $new_member_confirmation_template, $new_member_confirmation_title;
 
 $new_member_form_template = $PLUGIN_DIR . 'templates/new-member-form.php';
 $new_member_form_title = 'New Member';
-$confirmation_template = $PLUGIN_DIR . 'templates/new-member-confirmation.php';
+$new_member_confirmation_template = $PLUGIN_DIR . 'templates/new-member-confirmation.php';
+$new_member_confirmation_title = 'Submission accepted';
 
 // Define a function to create a custom page
 function add_new_member_form_page()
@@ -17,7 +18,7 @@ function add_new_member_form_page()
 		global $new_member_form_template, $new_member_form_title; 
 		// Create the page if it doesn't already exist
 		$page = get_page_by_title($new_member_form_title); 
-		if (!isset($page->ID) || $page->post_type != 'members') {
+		if (!isset($page->ID)) {
 				$page_title = $new_member_form_title;
 				$page_arr = array(
 						'post_type' 	 => 'page',
@@ -27,10 +28,8 @@ function add_new_member_form_page()
 				);
 	
 				$page_id = wp_insert_post($page_arr);
-				$template = $new_member_page_template;
-				if (!empty($template)) {
-						update_post_meta($page_id, '_wp_page_template', $template);
-				}
+				$template = $new_member_form_template;
+				update_post_meta($page_id, '_wp_page_template', $template);
 		}
 }
 
@@ -39,7 +38,7 @@ function add_new_member_confirmation_page()
 		global $new_member_confirmation_template, $new_member_confirmation_title;
 		// Create the page if it doesn't already exist
 		$page = get_page_by_title($new_member_confirmation_title); 
-		if (!isset($page->ID) || $page->post_type != 'members') {
+		if (!isset($page->ID)) {
 				$page_title = $new_member_confirmation_title;
 				$page_arr = array(
 						'post_type' 	 => 'page',
@@ -50,19 +49,16 @@ function add_new_member_confirmation_page()
 	
 				$page_id = wp_insert_post($page_arr);
 				$template = $new_member_confirmation_template;
-				if (!empty($template)) {
-						update_post_meta($page_id, '_wp_page_template', $template);
-				}
+				update_post_meta($page_id, '_wp_page_template', $template);
 		}
 }
  
 // Include a template for the new member page from the plugin
 function include_new_member_template($template)
 {
-		global $new_member_page_template, $new_member_page_title;
-		if (is_page($new_member_page_title)) {
-				return $new_member_page_template;
-		}
+		global $new_member_form_template, $new_member_form_title;
+		if (is_page($new_member_form_title))
+				return $new_member_form_template;
 		return $template;
 }
 
@@ -70,18 +66,17 @@ function include_new_member_template($template)
 function include_submit_confirmation_template($template)
 {
 		global $new_member_confirmation_template, $new_member_confirmation_title;
-		if (is_page($new_member_confirmation_title)) {
-				return $confirmation_template;
-		}
+		if (is_page($new_member_confirmation_title))
+				return $new_member_confirmation_template;
 		return $template;
 }
 		
 // Remove the page from the database
 function remove_new_member_pages()
 {
-		global $new_member_page_title; $new_member_confirmation_title;
+		global $new_member_form_title, $new_member_confirmation_title;
 		$form_page = get_page_by_title($new_member_form_title);
 		$confirmation_page = get_page_by_title($new_member_confirmation_title);
-		wp_delete_post($form_page->ID);
-		wp_delete_post($confirmation_page->ID);
+		wp_delete_post($form_page->ID, true);
+		wp_delete_post($confirmation_page->ID, true);
 }
