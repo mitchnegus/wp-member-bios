f7863f7e9159ffb1baee78a14f05480cf08901ec
Science Policy Group at Berkeley <sciencepolicy@ocf.berkeley.edu>
2019-05-18 18:45:14 -0700

    split main function for new member processing

--- includes/submit-new-members.php
+++ includes/submit-new-members.php
@@ -6,29 +6,47 @@
 // Define a function to accept user input
 function submit_new_member_form()
 {
-		$posted_nonce = $_POST['new_member_form_nonce'];
+		check_nonce();
+		$inputs = sanitize_input_fields();
+		$post_id = create_new_post($inputs);
+}
+
+function check_nonce()
+{
+		// Check that a nonce was provided and is valid, otherwise kill execution
+		$nonce = $_POST['new_member_form_nonce'];
 		$nonce_action = 'add_new_member_form_nonce';
-		if(isset($posted_nonce) && wp_verify_nonce($posted_nonce, $nonce_action)) {
-				// Sanitize input
-				$name = sanitize_text_field($_POST['name']);
-				$email = sanitize_email($_POST['email']);
-				$subject = sanitize_text_field($_POST['subject']);
-				$grad_date = sanitize_text_field($_POST['grad_date']);
-				$interests = sanitize_text_field($_POST['interests']);
-				$bio = sanitize_text_field($_POST['bio']);
-				// Set the array to insert for the new member's database entry
-				$post_arr = array(
-						'post_type'    => 'members',
-						'post_title'   => $name,
-						'post_content' => $bio,
-						'post_name'    => 'test-new-member'
-				);
-				// Insert a new post for the new member with accompanying metadata
-				$post_id = wp_insert_post($post_arr);
-				update_post_meta($post_id, 'subject', $subject);
-				update_post_meta($post_id, 'grad_date', $grad_date);
-				update_post_meta($post_id, 'interests', $interests);
-		} else {
+		if (!isset($nonce) || !wp_verify_nonce($nonce, $nonce_action)) {
 				wp_die('Invalid nonce specified', 'Error', array('response'=> 403));
 		}
+
+function sanitize_input_fields()
+{
+		// Sanitize all text input fields
+		$sanitized_inputs = array(
+				'name' 			=> sanitize_text_field($_POST['name']),
+				'email' 		=> sanitize_email($_POST['email']),
+				'subject' 	=> sanitize_text_field($_POST['subject']),
+				'grad_date' => sanitize_text_field($_POST['grad_date']),
+				'interests' => sanitize_text_field($_POST['interests']),
+				'bio'				=> sanitize_text_field($_POST['bio'])
+		);
+		return $sanitized_inputs;
+}
+
+function create_new_post($inputs)
+{
+		// Create a new post based on the user inputs
+		$post_arr = array(
+				'post_type'    => 'members',
+				'post_title'   => $inputs['name'],
+				'post_content' => $inputs['bio'],
+				'post_name'    => 'test-new-member'
+		);
+		// Insert a new post for the new member with accompanying metadata
+		$post_id = wp_insert_post($post_arr);
+		update_post_meta($post_id, 'subject', $inputs['subject']);
+		update_post_meta($post_id, 'grad_date', $inputs['grad_date']);
+		update_post_meta($post_id, 'interests', $inputs['interests']);
+		return $post_id;
 }
