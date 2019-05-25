b4f7d2c14b6ca8785af96a7649d6161293af004c
Science Policy Group at Berkeley <sciencepolicy@ocf.berkeley.edu>
2019-05-24 23:59:52 -0700

    updated deactivator class file

--- goal_framework/member-bios/includes/class-member-bios-deactivator.php
+++ goal_framework/member-bios/includes/class-member-bios-deactivator.php
@@ -20,17 +20,40 @@
  * @subpackage Member_Bios/includes
  * @author     Mitch Negus <mitchell.negus.57@gmail.com>
  */
+
+global $PLUGIN_DIR;
+global $new_member_form_title, $new_member_confirmation_title;
+
+$new_member_form_title = 'New Member';
+$new_member_confirmation_title = 'Submission accepted';
+
 class Member_Bios_Deactivator {
 
 	/**
-	 * Short Description. (use period)
+	 * Deactivate the plugin, removing the new members page and confirmation.
 	 *
-	 * Long Description.
+	 * Upon deactivation, this class handles the removal of the new membes page
+	 * and submission confirmation page from the database. (Pages were added to
+	 * the database upon activation.)
 	 *
 	 * @since    1.0.0
 	 */
-	public static function deactivate() {
 
+	public static function deactivate() {
+		Member_Bios_Deactivator::remove_new_member_form_page()
+		Member_Bios_Deactivator::remove_new_member_confirmation_page()
+	}
+
+	public static function remove_new_member_form_page(){
+		global $new_member_form_title;
+		$form_page = get_page_by_title( $new_member_form_title );
+		wp_delete_post( $form_page->ID, true );
+	}
+
+	public static function remove_new_member_confirmation_page(){
+		global $new_member_confirmation_title;
+		$confirmation_page = get_page_by_title( $new_member_confirmation_title );
+		wp_delete_post( $confirmation_page->ID, true );
 	}
 
 }
