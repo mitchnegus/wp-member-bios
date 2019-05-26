69d5c10c5bee5782eaa6a853d1c740ae944887bf
Science Policy Group at Berkeley <sciencepolicy@ocf.berkeley.edu>
2019-05-26 00:26:27 -0700

    migrated custom post creation to framework

--- goal_framework/member-bios/includes/class-member-bios.php
+++ goal_framework/member-bios/includes/class-member-bios.php
@@ -174,6 +174,9 @@ class Member_Bios {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		// Hook up our custom post to theme setup
+		$this->loader->add_action( 'init', $plugin_public, 'register_member_post_type' );
+		$this->loader->add_action( 'init', $plugin_public, 'register_positions_taxonomy');
 
 	}
 
--- goal_framework/member-bios/public/class-member-bios-public.php
+++ goal_framework/member-bios/public/class-member-bios-public.php
@@ -15,6 +15,13 @@
  *
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the public-facing stylesheet and JavaScript.
+ * Defines the plugin name, version, and hooks for managing the public front
+ * end (including enqueuing the admin-specific stylesheet and JavaScript). An
+ * instance of this class should be passed to the run() function defined
+ * in Member_Bios_Loader as all of the hooks are actually defined in that
+ * particular class. The Member_Bios_Loader will then create the
+ * relationship between the defined hooks and the functions defined in this
+ * class.
  *
  * @package    Member_Bios
  * @subpackage Member_Bios/public
@@ -61,18 +68,6 @@ class Member_Bios_Public {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Member_Bios_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Member_Bios_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style(
 			$this->member_bios,
 			plugin_dir_url( __FILE__ ) . 'css/member-bios-public.css',
@@ -90,25 +85,72 @@ class Member_Bios_Public {
 	 */
 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Member_Bios_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Member_Bios_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script(
 			$this->member_bios,
 			plugin_dir_url( __FILE__ ) . 'js/member-bios-public.js',
 			array( 'jquery' ),
 			$this->version,
 			false
-	 	);
+		);
+
+	}
+
+	/**
+	 * Register the custom post type for a member.
+	 *
+	 * Each group member has an individual post that stores their information
+	 * (name, expected graduation date, subject, interests, bio, etc.). This 
+	 * post is also accessed for display on the general members page, where 
+	 * all members of the organization are listed.
+	 *
+	 * @since    1.0.0
+	 */
+	public function register_member_post_type() {
+
+		$labels = array(
+			'name' 					=> __('Members'),
+			'singular_name' => __('Member'),
+			'add_new_item' 	=> __('Add New Member'),
+			'edit_item' 		=> __('Edit Member')
+		);
+
+		$args = array(
+			'labels' 			=> $labels,
+			'public'			=> true,
+			'has_archive' => true,
+			'rewrite' 		=> array('slug' => 'members'),
+			'supports' 		=> array('title', 'editor', 'thumbnail'),
+			'menu_icon' 	=> 'dashicons-groups'
+		);
+
+		register_post_type('members', $args);
+		flush_rewrite_rules();
+
+	}
+
+	/**
+	 * Register the positions taxonomy for a member post.
+	 *
+	 * Each group member can hold many different positions in the group.
+	 * This taxonomy allows positions like general member, executive, co-founder
+	 * or alumni to be included for any given member.
+	 *
+	 * @since    1.0.0
+	 */
+	public function register_positions_taxonomy() {
+
+  	$labels = array(
+				'name' 					=> __('Positions'),
+				'singular_name' => __('Position'),
+				'add_new_item' 	=> __('Add New Position')
+		);
+
+		$args = array(
+				'labels' 	=> $labels,
+				'rewrite'	=> array('slug' => 'positions')
+		);
+
+		register_taxonomy('positions', array('members'), $args);
 
 	}
 
