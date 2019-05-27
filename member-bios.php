6a43a910c266fbcc2bcebc19af02c68e73346a1b
Science Policy Group at Berkeley <sciencepolicy@ocf.berkeley.edu>
2019-05-27 01:09:02 -0700

    removing old max headshot size code

--- member-bios.php
+++ member-bios.php
@@ -23,35 +23,10 @@ global $PLUGIN_DIR, $PLUGIN_URL;
 $PLUGIN_DIR = plugin_dir_path(__FILE__);
 $PLUGIN_URL = plugin_dir_url(__FILE__);
 
-// Set the maximum image size to be allowed in an upload
-global $max_headshot_size;
-$max_headshot_size = set_plugin_max_headshot_size();
-
 // Plugin specific files to include
 require($PLUGIN_DIR . '/includes/define-members-post-type.php');
 
-// These files need to be included as dependencies when on the front end.
-require_once(ABSPATH . 'wp-admin/includes/image.php');
-require_once(ABSPATH . 'wp-admin/includes/file.php');
-require_once(ABSPATH . 'wp-admin/includes/media.php');
- 
-// Set the maximum image size for user uploaded headshots
-function set_plugin_max_headshot_size()
-{
-		$max_wordpress_upload_size = wp_max_upload_size();
-		if ($max_wordpress_upload_size > 1e6) {
-    		$plugin_max_headshot_size = 1e6;   // (Approx. 1 MB)
-    } else {
-        $plugin_max_headshot_size = $max_wordpress_upload_size;
-    }
-		return $plugin_max_headshot_size;
-}
-
 require $PLUGIN_DIR . '/goal_framework/member-bios/member-bios.php';
-
-// Add theme support for thumbnails if not already included
-add_theme_support('post-thumbnails');
-set_post_thumbnail_size(200, 200);
 
 // Add new member submission page on activation
 register_activation_hook(__FILE__, 'activate_member_bios');
