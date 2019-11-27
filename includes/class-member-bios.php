2bbc51f1972b1dc8a50019a0765d7c53b616ec4d
Science Policy Group at Berkeley <sciencepolicy@ocf.berkeley.edu>
2019-11-27 11:17:16 -0800

    fixed typo in hook

--- includes/class-member-bios.php
+++ includes/class-member-bios.php
@@ -225,7 +225,7 @@ class Member_Bios {
 		$this->loader->add_action( 'init', $plugin_public, 'register_positions_taxonomy');
 		// Accept user input
 		$this->loader->add_action( 'admin_post_submit_member', $plugin_public, 'submit_new_member_form' );
-		$this->loader->add_action( 'admin_nopriv_post_submit_member', $plugin_public, 'submit_new_member_form' );
+		$this->loader->add_action( 'admin_post_nopriv_submit_member', $plugin_public, 'submit_new_member_form' );
 		// Use a custom template for the new member and submission success pages
 		$this->loader->add_filter( 'template_include', $plugin_public, 'include_new_member_template' );
 		$this->loader->add_filter( 'template_include', $plugin_public, 'include_submit_confirmation_template' );
